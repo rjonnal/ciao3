b9358c67fcab7ce37a9191ddbd756a49c9a44496
Ravi Sankar Jonnal <rjonnal@gmail.com>
2021-01-07 20:46:19 -0800

    added larger data to producer_consumer example, to test whether AO data (SHWS image, wavefront, other metrics) could be passed easily back and forth

--- tutorials/producer_consumer/main.py
+++ tutorials/producer_consumer/main.py
@@ -4,11 +4,16 @@ import logging
 import random
 import queue
 import numpy as np
+from dearpygui import core, simple
 
 logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-9s) %(message)s',)
 
 BUF_SIZE = 10
+N = 256
+
+
+
 
 class ProducerThread(threading.Thread):
     def __init__(self, q, group=None, target=None, name=None,
@@ -17,15 +22,22 @@ class ProducerThread(threading.Thread):
         self.target = target
         self.name = name
         self.q = q
-        
+        self.t0 = time.time()
+        self.steps = 0
+        self.fps = 0.0
     def run(self):
         while True:
             if not self.q.full():
-                item = np.sum(np.random.randn(1000000))
+                item = np.round(np.random.rand(N,N)).astype(np.uint8)
+                f = np.fft.fft2(item)
                 self.q.put(item)
-                logging.debug('Putting ' + '%0.0f'%item  
-                              + ' : ' + str(self.q.qsize()) + ' items in queue')
-                time.sleep(random.random())
+                logging.debug('Putting ' + '%0.1f'%item[0,0]
+                              + ' : ' + str(self.q.qsize()) + ' items in queue'
+                              + ' : ' + '%0.1f fps'%self.fps)
+                self.steps = self.steps + 1
+                self.dt = time.time()-self.t0
+                self.fps = float(self.steps)/self.dt
+                
         return
 
 class ConsumerThread(threading.Thread):
@@ -35,25 +47,42 @@ class ConsumerThread(threading.Thread):
         self.target = target
         self.name = name
         self.q = q
-
+        self.t0 = time.time()
+        self.steps = 0
+        self.fps = 0.0
+        
     def run(self):
         while True:
             if not self.q.empty():
                 item = self.q.get()
-                logging.debug('Getting ' + '%0.0f'%item
-                              + ' : ' + str(self.q.qsize()) + ' items in queue')
-                time.sleep(random.random())
-        return
+                logging.debug('Getting ' + '%0.1f'%item[0,0]
+                              + ' : ' + str(self.q.qsize()) + ' items in queue'
+                              + ' : ' + '%0.1f fps'%self.fps)
+                self.steps = self.steps + 1
+                self.dt = time.time()-self.t0
+                self.fps = float(self.steps)/self.dt
 
 if __name__ == '__main__':
 
     systems = []
-    for k in range(10):
+    for k in range(1):
         q = queue.Queue(BUF_SIZE)
         p = ProducerThread(q,name='producer %02d'%k)
         c = ConsumerThread(q,name='consumer %02d'%k)
         systems.append((p,c,q))
 
+
+    def save_callback(sender, data):
+        print("Save Clicked")
+
+    with simple.window("Example Window"):
+        core.add_text("Hello world")
+        core.add_button("Save", callback=save_callback)
+        core.add_input_text("string")
+        core.add_slider_float("float")
+
+    core.start_dearpygui()
+        
     for idx,system in enumerate(systems):
         logging.debug('Starting system %d.'%idx)
         system[0].start()
